e3c25df3f80f5d8436d7ef321fd692fc327fe9d5
Aswin-B <aswinbalamurugan0401@gmail.com>
2025-12-01 10:19:16 +0530

    Java codes

--- src/If_else_statements.java
+++ src/If_else_statements.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class If_else_statements {
     public static void main(String[] args) {
-        System.out.println("If else statements");
+        // System.out.println("If else statements");
+
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        num = scanner.nextInt();
+
+        if(num > 0){
+            System.out.println("The given number is +ve");
+        }
+        else{
+            System.out.println("The given number is -ve");
+        }
     }
 }
